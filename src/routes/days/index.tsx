5f0ff0e579ec605a2aaee8cdd57fd44f5b85ada3
Adrian Lafond <mail@adrianlafond.com>
2022-05-28 14:40:15 -0400

    attempt dropbox from client

--- src/routes/days/index.tsx
+++ src/routes/days/index.tsx
@@ -1,15 +1,23 @@
 import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
+import { Dropbox, DropboxAuth } from 'dropbox';
+import { openTodosFile, getDefaultTodos } from '../../services';
+import { parseQueryString } from '../../utils/parse-query-string';
 
 import style from './style.css';
-import { openTodosFile, getDefaultTodos } from '../../services';
 
 export interface DaysProps {
   date?: string;
 }
 
+const CLIENT_ID = 'ih3d422ruazvhwd';
+
 const Days = ({ date }: DaysProps) => {
   const [days, setDays] = useState(getDefaultTodos());
+  const [auth, setAuth] = useState({
+    isAuthenticated: false,
+    url: '',
+  });
 
   const handleOpenFile = async () => {
     const data = await openTodosFile();
@@ -18,6 +26,40 @@ const Days = ({ date }: DaysProps) => {
     }
   };
 
+  async function listFiles(accessToken: string) {
+    const dbx = new Dropbox({ accessToken });
+    try {
+      const files = await dbx.filesListFolder({ path: '' });
+      // eslint-disable-next-line no-console
+      console.log(files);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }
+
+  async function setupAuth() {
+    const dbxAuth = new DropboxAuth({ clientId: CLIENT_ID });
+    const url = (await dbxAuth.getAuthenticationUrl(
+      'http://localhost:8080'
+    )) as string; // because "Type 'String' is not assignable to type 'string'"
+    setAuth({
+      isAuthenticated: true,
+      url,
+    });
+  }
+
+  useEffect(() => {
+    const params = parseQueryString(window.location.hash);
+    const token = params.access_token as string;
+    const isAuthenticated = !!token;
+    if (isAuthenticated) {
+      listFiles(token);
+    } else {
+      setupAuth();
+    }
+  }, [typeof window !== 'undefined' ? window.location.href : null]);
+
   return (
     <div className={style.days}>
       <h1>{days.title}</h1>
@@ -25,7 +67,17 @@ const Days = ({ date }: DaysProps) => {
       <h2>{date}</h2>
       <p>This is the Days component.</p>
 
-      <button onClick={handleOpenFile}>Open file</button>
+      <button onClick={handleOpenFile}>Open local file</button>
+
+      <hr />
+
+      <h2>Dropbox</h2>
+
+      {auth.isAuthenticated ? (
+        <a href={auth.url}>Authenticate with Dropbox</a>
+      ) : (
+        <p>Congrats</p>
+      )}
     </div>
   );
 };
---
+++ src/utils/parse-query-string.ts
@@ -0,0 +1,43 @@
+type Query = string | null | (string | null | Query)[];
+
+/**
+ * Updated from https://github.com/dropbox/dropbox-sdk-js/blob/main/examples/javascript/utils.js
+ */
+export function parseQueryString(queryStr: string): {
+  [key: string]: Query;
+} {
+  const result: { [key: string]: Query } = {};
+
+  const str = queryStr.trim().replace(/^(\?|#|&)/, '');
+
+  if (!queryStr) {
+    return result;
+  }
+
+  str.split('&').forEach(param => {
+    const parts = param.replace(/\+/g, ' ').split('=');
+    // Firefox (pre 40) decodes `%3D` to `=`
+    // https://github.com/sindresorhus/query-string/pull/37
+    let key = parts.shift();
+
+    if (key) {
+      key = decodeURIComponent(key);
+      const rawValue = parts.length > 0 ? parts.join('=') : undefined;
+
+      // missing `=` should be `null`:
+      // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+      const value =
+        rawValue === undefined ? null : decodeURIComponent(rawValue);
+
+      if (result[key] === undefined) {
+        result[key] = value;
+      } else if (Array.isArray(result[key])) {
+        (result[key] as Array<string | null>).push(value);
+      } else {
+        result[key] = [result[key], value];
+      }
+    }
+  });
+
+  return result;
+}
